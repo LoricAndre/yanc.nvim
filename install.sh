96c5133c71a7ea5478bf9225ee8778c65886341a
Loric ANDRE <gwynaark@gmail.com>
2021-07-20 18:43:48 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-if [ -z "$XDG_CONFIG_DIR" ] then
-  config_dir = "$XDG_CONFIG_DIR"
+if [ -z "$XDG_CONFIG_DIR" ] 
+then
+  config_dir="$HOME/.config"
 else
-  config_dir = "$HOME/.config"
+  config_dir="$XDG_CONFIG_DIR"
 fi
-mv -r "$config_dir/nvim" "$config_dir/nvim_bak" && \
+mv "$config_dir/nvim" "$config_dir/nvim_bak" && \
   git clone https://github.com/LoricAndre/yanc.nvim \
   "$config_dir/nvim"
 
