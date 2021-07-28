922eab33b800d97ff69d8e128340c4892d854e57
Loric ANDRE <gwynaark@gmail.com>
2021-07-28 14:40:18 +0200

    Startify config

--- lua/plugins/startify.lua
+++ lua/plugins/startify.lua
@@ -1,7 +1,12 @@
 vim.g.startify_lists = {
-  {type = 'files', header = 'Recent Files'},
-  {type = 'dir', header = 'Recent Files in ' .. vim.fn.getcwd()},
-  {type = 'sessions', header = 'Saved sessions'}
+  {type = 'commands', header = {'Commands'}},
+  {type = 'files', header = {'Recent Files'}},
+  {type = 'dir', header = {'Recent Files in ' .. vim.fn.getcwd()}},
+  {type = 'sessions', header = {'Saved sessions'}}
+}
+
+vim.g.startify_commands = {
+  {'Load Session', ':RestoreSession'}
 }
 
 vim.g.startify_session_dir = vim.fn.stdpath('data') .. '/sessions'
